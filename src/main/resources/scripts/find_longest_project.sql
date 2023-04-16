1d8839c6b5e92f36f3ab065a4c83805a783657e9
lena <ostapets.olena@gmail.com>
2023-04-16 22:45:23 +0300

    code review, fixed

--- src/main/resources/scripts/find_longest_project.sql
+++ src/main/resources/scripts/find_longest_project.sql
@@ -1,2 +1,2 @@
-select id, start_date, finish_date from project
+select id, DATEDIFF('MONTH', start_date, finish_date) as month_count from project
 where DATEDIFF('MONTH', start_date, finish_date) = select max(DATEDIFF('MONTH', start_date, finish_date)) as LongestProject from project;
--- src/main/resources/scripts/print_project_prices.sql
+++ src/main/resources/scripts/print_project_prices.sql
@@ -1,5 +1,4 @@
-select sum(w.salary), pw.project_id, DATEDIFF('MONTH', p.start_date, p.finish_date),
-(sum(w.salary)*DATEDIFF('MONTH', p.start_date, p.finish_date)) as price
+select pw.project_id, (sum(w.salary)*DATEDIFF('MONTH', p.start_date, p.finish_date)) as price
 from worker w
 left outer join project_worker pw
 on w.id = pw.worker_id
